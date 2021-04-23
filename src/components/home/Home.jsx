3ebc86a6b14e6b505cd769971d71fb37e141a653
Ángela Herrador <angelaherrador1991@gmail.com>
2021-04-23 18:54:52 +0200

    Finish views home

--- src/components/home/Home.jsx
+++ src/components/home/Home.jsx
@@ -2,23 +2,30 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 import './Home.css';
 import Navbar from '../Navbar/Navbar'
+import { useUser } from '../../hooks/userUserContext';
 
 const Home = () => {
 
-	//Meter contexto
+	const {user} = useUser()
 	
 	return (
 		<div className="Home">
-			user ? 
+			{
+			user ?
 				(
+				<>	
 				<h1>RunEat</h1>
-				<Navbar/>
+				<Navbar />
+				</>
 			)
 			: (
+				<>
 				<h1>RunEat</h1>
 				<button className="btn btn-primary mx-1"><Link className="text-white" to="/signup">Sign up</Link></button>
 				<button className="btn btn-primary mx-1"><Link className="text-white" to="/login">Log in</Link></button>
+				</>
 			)
+			}
 		</div>	
 	);
 };
