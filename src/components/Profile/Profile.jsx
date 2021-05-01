1f4b883570b3fcca02b48ce4a60c06718dcc7f8f
Ángela Herrador <angelaherrador1991@gmail.com>
2021-05-01 19:37:36 +0200

    Navbar desing done

--- src/components/Profile/Profile.jsx
+++ src/components/Profile/Profile.jsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import { useUser } from '../../hooks/useUserContext';
 import { Link, Redirect } from 'react-router-dom';
+import Navbar from '../Navbar/Navbar';
 
 function Profile() {
 
@@ -11,32 +12,37 @@ function Profile() {
     ) : !user.avatar ? (
       <Redirect to="/profile/edit" />
     ) : (
-      <div className="Profile">
+      <div className="Profile container">
         {user ? (
-          <div className="card" style={{ width: "18rem" }}>
-            <div className="card-body">
-              <img
-                src={user.avatar}
-                className="card-img-top"
-                alt={user.username}
-              />
-              <h5 className="card-title">Username: {user.username}</h5>
-              <p className="card-text">Email: {user.email}</p>
-              <p className="card-text">Age: {user.age}</p>
-              <p className="card-text">Height: {user.height}</p>
-              <p className="card-text">Weight: {user.weight}</p>
-              <p className="card-text">Activity level: {user.activity}</p>
-              <Link to="/profile/edit" className="btn btn-primary">
-                Edit Profile
-              </Link>
-              <Link to="/profile/delete" className="btn btn-primary">
-                Delete Account
-              </Link>
-            </div>
-            <Link to="/" className="btn btn-secondary">
-              Go to Home
+          <>
+          <div className="InfoProfile">
+            <img
+              src={user.avatar}
+              className="card-img-top"
+              alt={user.username}
+            />
+            <h5 className="card-title">Username: {user.username}</h5>
+            <p className="card-text">Activity level: {user.activity}</p>
+          </div>
+              
+          <div className="InfoBody">
+            <p className="card-text">Age: {user.age}</p>
+            <p className="card-text">Height: {user.height}</p>
+            <p className="card-text">Weight: {user.weight}</p>
+          </div>
+                
+          <div className="Settings">
+            <p className="card-text">Email: {user.email}</p>
+            <Link to="/profile/edit" className="btn btn-primary">
+              Edit Profile
+            </Link>
+            <Link to="/profile/delete" className="btn btn-primary">
+              Delete Account
             </Link>
           </div>
+                
+            <Navbar/>
+          </>
         ) : (
           <p>loading...</p>
         )}
